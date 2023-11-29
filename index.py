0616f56aa450b4c051b10cb96cb84030864dde4b
santoslopez <santos.lopez@galileo.edu.gt>
2023-11-28 22:22:48 -0600

    a

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("main/index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
