966fbcf662f83d0aee5b993518b5a8e5ef611999
santoslopez <santos.lopez@galileo.edu.gt>
2023-11-28 23:04:02 -0600

    Update index.py

--- servidor/index.py
+++ servidor/index.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify,g,redirect,url_for,session,flash
 from flask_socketio import SocketIO
-from errores import ErrorHandlers
+#from errores import ErrorHandlers
 #from viewsVecino import VecinosView
 #from enviarMensajes import *
 from auth import auth
@@ -32,40 +32,85 @@ app.register_blueprint(auth)
 
 #app.register_blueprint(ViewMensajes)
 
-socketio = SocketIO(app)
-ErrorHandlers.register(app,socketio)
+#socketio = SocketIO(app)
+#ErrorHandlers.register(app,socketio)
 reading_thread = None
 
-'''
-@app.route('/listarMensajes',methods=['GET','POST'])
-def listarMensajes():
-    idUsuarioSes = session.get("idUsuario")
-    convertirIDSesion = str(idUsuarioSes)
-    
-    if not idUsuarioSes:
-        # No hay una sesión activa, manejar esto según tus necesidades
-        flash("Inicia sesión para acceder a tus mensajes", "info")
-        registroLog.info("Necesitas iniciar sesion para acceder al metodo /listarMensajes.")
-
-        return redirect(url_for('login'))
-
+# realizar la consulta de login en sqlite 
+def queryLogin(usuario,passwordGuardar):
     conexion = sqlite3.connect(nombreBaseDatos)
-    cursor = conexion.cursor() 
-    cursor.execute("SELECT * FROM mensajes WHERE idUsuario=?",(convertirIDSesion))
-
-    mensajes = cursor.fetchall()
+    cursor = conexion.cursor()  
+    cursor.execute("select * from usuarios where usuario=? and password=?",(usuario,passwordGuardar,))
+    ejecutar = cursor.fetchall()
     conexion.close()
-    formatoJSON = [{'id':mensaje[0],'tipoMensaje':mensaje[1],'mensaje':mensaje[2],'fecha':mensaje[3],'idUsuario':mensaje[4]} for mensaje in mensajes]
+    return ejecutar
+
+@app.route('/login',methods=['POST'])
+def login():
+    if "usuarioSesion" not in session:
+        return redirect(url_for('index'))
     
-    return jsonify({'data': formatoJSON})
-@app.route('/listaPuertos',methods=['GET','POST'])
-def listaPuertos():
-    puertos = obtenerPuertosCOM()    
-    if request.method =='POST':
-        return render_template("main/puerto.html", puertosCOM=puertos)
-    return render_template("main/puerto.html", puertosCOM=puertos)
-'''
+    if request.method == 'POST':
+        usuario = request.form.get("txtUsuario")
+        passwordGuardar = request.form.get("txtPassword")
+        ejecutar = queryLogin(usuario,passwordGuardar)
+
+        # validar que el usuario y password sean correctos
+        if len(ejecutar) > 0:
+            # se crea la sesion de que existe el usuario
+            session["usuarioSesion"]=usuario
+            # se accede al primer resultado y primera columna
+            session["idUsuario"]=ejecutar[0][0]
+            usuarioSes = session["usuarioSesion"]
+            idUsuarioSes = session["idUsuario"]
+            flash("Inicio de sesion exitoso", "success")
+            return render_template("main/index.html",registroExitoso=True,usuarioSesion=usuarioSes,idUsuario=idUsuarioSes)
+        else:
+            flash("Usuario o contraseña incorrectos","error")
+            return render_template("auth/login.html",registroExitoso=False)
+
+       
+@app.route('/fmrLogin',methods=['GET','POST'])
+def frmLogin():
+    if "usuarioSesion" in session:
+        idUsuarioSes = session["idUsuario"]
+        return render_template("main/index.html",idUsuario=idUsuarioSes,usuarioSesion=session["usuarioSesion"])
+    else:
+        flash("No has iniciado sesión")
+        registroLog.info("El usuario no ha iniciado sesion redirigido al login.")
+        return render_template("auth/login.html")
     
+@app.route('/frmRegistroCuenta',methods=['GET','POST'])
+def frmRegistroCuenta():
+    return render_template("auth/frmRegistroCuenta.html")
+
+@app.route('/registroCuenta',methods=["POST"])
+def registroCuenta():
+    if request.method == "POST":
+        usuarioGuardar = request.form.get("txtUsuario")
+        passwordGuardar = request.form.get("txtPassword")
+        
+        if not usuarioGuardar or not passwordGuardar:
+            flash("Debes ingresar un usuario y una contraseña","error")
+            return render_template("auth/frmRegistroCuenta.html")
+        else:
+            with sqlite3.connect(nombreBaseDatos) as conexion:
+                cursor = conexion.cursor()
+                cursor.execute("INSERT INTO usuarios(usuario,password) VALUES (?, ?)",(usuarioGuardar,passwordGuardar))
+                conexion.commit()
+                registroLog.info("El usuario se ha guardado correctamente.")
+
+                return render_template("auth/registroExitoso.html")
+    else:
+        return render_template("auth/frmRegistroCuenta.html")
+
+
+# cerrar la sesion del usuario
+@app.route('/logout',methods=['GET','POST'])
+def logout():
+    session.pop("usuarioSesion",None)
+    flash("Se ha cerrado la sesion correctamente","sucess")
+    return redirect(url_for("auth.frmLogin"))
 
 @app.route('/',methods=['GET','POST'])
 def index():
@@ -76,17 +121,5 @@ def index():
         flash("No has iniciado sesión")
     return render_template("auth/login.html")
 
-'''
-@socketio.on('connect')
-def handle_connect():
-    print('Cliente conectado')
-    global reading_thread
-    if reading_thread is None or not reading_thread.is_alive():
-        reading_thread = threading.Thread(target=leerPuerto())
-        reading_thread.start()
-    else:
-        print('El hilo ya está en ejecución')
-'''
-        
 if __name__ == '__main__':
     app.run(debug=True)
