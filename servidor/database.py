e93ccad77001803531c7db01454525e773be5049
santoslopez <santos.lopez@galileo.edu.gt>
2023-11-28 22:39:30 -0600

    a

--- servidor/database.py
+++
@@ -1,4 +0,0 @@
-'''
-Nombre de la base de datos de sqlite que se esta utilizando en el proyecto
-'''
-nombreBaseDatos = "database.db"
--- servidor/logger.py
+++
@@ -1,9 +0,0 @@
-'''
-Archivo para configurar el registro de logs de la aplicacion
-'''
-import logging
-nombreLog = "app.log"
-# configurar registro de sistema
-logging.basicConfig(filename=nombreLog,level=logging.DEBUG)
-# realizar el registro de los errores
-registroLog = logging.getLogger("index")
